6caac390f93db2dde5f1bcd62375a52867ed26a8
Ronny Trommer <ronny@opennms.org>
2018-07-15 12:12:20 +0200

    Split in functions to build and build and run

--- docker-build.sh
+++ docker-build.sh
@@ -4,6 +4,9 @@
 
 DOCKER_IMAGE=opennms/build-env:latest
 GIT_URL=https://github.com/OpenNMS/opennms.git
+DISPLAY_VERSION=SNAPSHOT
+INSTALL_VERSION=opennms-SNAPSHOT
+BRANCH=develop
 
 # Error codes
 E_ILLEGAL_ARGS=126
@@ -14,14 +17,29 @@ usage() {
   echo ""
   echo "Docker build script for parallel builds of branches using a Docker build image"
   echo ""
-  echo "-b: Name of the branch to build, default is \"develop\""
+  echo "-b: Name of the branch to build"
+  echo "-s: Name of the branch to build and launch using docker-compose"
   echo "-h: Show this help."
   echo ""
 }
 
-build() {
-  local BRANCH=${1}
-  local BRANCH=${BRANCH/\//-}
+checkRequirements() {
+  docker -v
+  if [ ! $? -eq 0 ]; then
+    echo "Docker is not installed or in search path."
+    exit E_ILLEGAL_ARGS
+  fi
+
+  docker-compose -v
+  if [ ! $? -eq 0 ]; then
+    echo "Docker compose is not installed or in search path."
+    exit E_ILLEGAL_ARGS
+  fi
+}
+
+getBranch() {
+  BRANCH=${1}
+  BRANCH=${BRANCH/\//-}
 
   if [ ! -d ${BRANCH} ]; then
     mkdir ${BRANCH}
@@ -36,16 +54,24 @@ build() {
     git checkout origin/${1}
     git pull origin ${1}
   fi
+  cd ../..
+}
+
+versioning() {
+  cd ${BRANCH}/opennms
+
+  # Version number built in the target directory
+  INSTALL_VERSION=$(grep '<version>' pom.xml | sed -e 's,^[^>]*>,,' -e 's,<.*$,,' | head -n 1)
+  INSTALL_VERSION=opennms-${INSTALL_VERSION}
 
   # Identify version to display you try to compile and extend version number with branch
   DISPLAY_VERSION=$(grep '<version>' pom.xml | sed -e 's,^[^>]*>,,' -e 's,<.*$,,' -e 's,-[^-]*-SNAPSHOT$,,' -e 's,-SNAPSHOT$,,' -e 's,-testing$,,' -e 's,-,.,g' | head -n 1)
   DISPLAY_VERSION=${DISPLAY_VERSION}-${BRANCH}
   DISPLAY_VERSION=${DISPLAY_VERSION}-$(git describe)
-  
-  # Version number built in the target directory
-  INSTALL_VERSION=$(grep '<version>' pom.xml | sed -e 's,^[^>]*>,,' -e 's,<.*$,,' | head -n 1)
-  
   cd ../..
+}
+
+build() {
   docker run --rm \
     -l "branch=${1}" \
     -v $(pwd)/${BRANCH}/opennms:/usr/src/opennms \
@@ -59,7 +85,7 @@ build() {
     -v $(pwd)/m2:/root/.m2 \
     -v $(pwd)/${BRANCH}/m2:/root/.m2/repository/org/opennms \
     ${DOCKER_IMAGE} /usr/bin/perl ./assemble.pl -DskipTests -p dir -Dopennms.home=/opt/opennms
-  
+
   # Use etc-overlay directory to set a custom version number displayed in the Web UI
   if [ ! -d ${BRANCH}/etc-overlay/opennms-properties.d ]; then
     mkdir -p ${BRANCH}/etc-overlay/opennms.properties.d
@@ -69,7 +95,11 @@ build() {
   cd ${BRANCH}
   cp ../docker-compose.tpl docker-compose.yml
   sed -i "s/INSTALL_VERSION/${INSTALL_VERSION}/g" docker-compose.yml
+}
+
+run() {
   docker-compose up -d
+  docker ps
 }
 
 # Evaluate arguments for build script.
@@ -79,10 +109,21 @@ if [[ "${#}" == 0 ]]; then
 fi
 
 # Evaluate arguments for build script.
-while getopts "b:h" flag; do
+while getopts "b:hs:" flag; do
   case ${flag} in
     b)
-      build ${OPTARG}
+      checkRequirements
+      getBranch ${OPTARG}
+      versioning
+      build
+      exit
+      ;;
+    s)
+      checkRequirements
+      getBranch ${OPTARG}
+      versioning
+      build
+      run
       exit
       ;;
     h)
@@ -105,4 +146,3 @@ if [[ "${#}" > 0 ]]; then
   usage
   exit ${E_ILLEGAL_ARGS}
 fi
-
